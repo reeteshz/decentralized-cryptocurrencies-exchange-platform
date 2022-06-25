f32267e87d8fc22bff1956e90d352feb3984246a
reetesh13 <reeteshzope@gmail.com>
2022-06-25 16:29:05 -0400

    Transaction logs functionality implemented

--- client/components/LogsTable.js
+++ client/components/LogsTable.js
@@ -1,23 +1,33 @@
 import { useEffect, useState, useContext } from 'react'
 import { TransactionContext } from '../context/TransactionContext'
 
+const style = {
+    tableRow: `bg-[#000000] px-4 py-2 my-2 flex justify-around`
+  }
+
 const LogsTable = () => {
     const [data, setData] = useState(null)
     const [isLoading, setLoading] = useState(false)
     const { currentAccount } = useContext(TransactionContext)
-    console.log(currentAccount)
+    const startBlock = 0
+    const endBlock = 99999999
     const etherScanURL = "https://rinkeby.etherscan.io/tx/"
+    const APIURL = "https://api-rinkeby.etherscan.io/api"
+    const divider = 1000000000000000000
+    const logLimit = 5
+
     useEffect(() => {
-        if (!data) {
+        if (!data && typeof(currentAccount) != "undefined") {
             setLoading(true)
-            fetch('https://api-rinkeby.etherscan.io/api?module=account&action=txlist&address=0xDA500C9444b7c9A376758086C1573755D69F53Ee&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=RTH8QBD64WAMZG8G5ZTIXJCB6MMA7IGUYR')
+            URL = `${APIURL}?module=account&action=txlist&address=${currentAccount}&startblock=${startBlock}&endblock=${endBlock}&page=1&offset=${logLimit}&sort=desc&apikey=${process.env.ETHERSCAN_APIKEY}`
+            fetch(URL)
             .then((res) => res.json())
             .then((data) => {
                 setData(data)
                 setLoading(false)
             })
         }
-    }, [])
+    }, [currentAccount])
 
     if (isLoading) return <p>Fetching Transaction Logs...</p>
     if (!data) return <p>No profile data</p>
@@ -27,10 +37,16 @@ const LogsTable = () => {
         <h4>Logs Table</h4>
         {data.result.length > 0 && (
             <ul>
-            {data.result.reverse().map(tras => (
-                <p key={Math.random()}>
-                    <a key={Math.random()} href={etherScanURL + tras.hash} target="_blank" rel="noreferrer">{tras.hash}</a>
-                </p>
+            {data.result.map((thrans, index) => (
+                <li key={Math.random()} className={style.tableRow}>
+                    <div className={style.tableRow}>
+                        <div> { index + 1 } </div>
+                        <div>{thrans.hash}</div> 
+                        <div> { thrans.value / divider} </div>
+                        <div><a key={Math.random()} href={etherScanURL + thrans.hash} target="_blank" rel="noreferrer">View on EtherScan</a> </div>
+                    </div>
+                    
+                </li>
             ))}
             </ul>
         )}
